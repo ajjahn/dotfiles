e16a1a6c3cbefbaa790f4246261e67d1f5b19350
Adam Jahn <ajjahn@gmail.com>
2021-06-04 11:25:20 -0400

    brew updates, improves install

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Install Homebrew"
+if test ! "$(which brew)"; then
+  echo "Installing homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install brew formulas in Brewfile
+echo "Install Homebrew packages"
+set +e
 brew bundle --file="$(dirname "$0")/Brewfile"
+set -e
 
 # Install dotfiles
 rcup
