4f44e345a69bbeb3cb8117f511b6bd3d64a19c71
Adam Jahn <ajjahn@gmail.com>
2023-06-27 09:46:03 -0400

    key bindings for trouble

--- config/nvim/lua/_trouble.lua
+++ config/nvim/lua/_trouble.lua
@@ -3,6 +3,6 @@ require("trouble").setup {
   -- or leave it empty to use the default settings
   -- refer to the configuration section below
 }
-vim.keymap.set("n", "hd", "<cmd>TroubleToggle  document_diagnostics<cr>",
+vim.keymap.set("n", "gh", "<cmd>TroubleToggle  document_diagnostics<cr>",
   { silent = true, noremap = true }
 )
